a85e60a7caf6d8dae03559147d1f18bf9be4f859
adnan-nagori <adi.developer162@gmail.com>
2021-09-20 16:05:45 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms'  
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+  name = 'Test Form';  
+    
+  testForm: FormGroup;  
+  
+
+  constructor(
+    private fb:FormBuilder
+  ) { 
+    this.testForm = this.fb.group({  
+      name: '',  
+      adfields: this.fb.array([]) ,  
+    });
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  adfields() : FormArray {  
+    return this.testForm.get("adfields") as FormArray  
+  } 
+
+  newQuantity(): FormGroup {  
+    return this.fb.group({  
+      Address: ''
+       
+    })  
+  }  
+
+  addFields() {  
+    this.adfields().push(this.newQuantity());  
+  }  
+     
+  removeFields(i:number) {  
+    this.adfields().removeAt(i);  
+  }  
+     
+  onSubmit() {  
+    console.log(this.testForm.value);  
+  }  
+
+  
+  
+
+}
